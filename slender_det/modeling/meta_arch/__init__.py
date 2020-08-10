5c7feaf2f905cb4acc6cad4f34076a9a1f046dbd
dengsutao <dengsutao@dengsutao_baseline.dengsutao.ws2.wh-a.brainpp.cn>
2020-08-10 12:48:18 +0800

    fcos+reppoints+s2_topk

--- slender_det/modeling/meta_arch/__init__.py
+++ slender_det/modeling/meta_arch/__init__.py
@@ -21,8 +21,8 @@ from .corner_net import CornerNet
 
 #from .fcos_rpd import FCOSRepPoints
 #from .fcos_dcn import FCOSRepPoints
-from .fcos_rpd_s1_topk import FCOSRepPoints
-#from .fcos_rpd_s2_topk import FCOSRepPoints
+#from .fcos_rpd_s1_topk import FCOSRepPoints
+from .fcos_rpd_s2_topk import FCOSRepPoints
 #from .fcos_rpd_dcnv2 import FCOSRepPoints
 #from .fcos_rpd_conv_points import FCOSRepPoints
 #from .fcos_rpd_mask_assign import FCOSRepPoints
